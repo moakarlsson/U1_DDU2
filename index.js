9005ae5f42ad3c1908598f80a6c63ae5cfe7f87e
Moakarlssonns <mo-karlsson@hotmail.com>
2024-11-19 13:03:49 +0100

    Added for of loop (cities)
    Declared a for of loop and made a innerHTML to add the cities and classes to the website.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Recommended: All functions declared here
 
+
 // Recommended: constants with references to existing HTML-elements
+const citiesDiv = document.getElementById("cities");
 const main = document.querySelector("main");
 const h1 = document.querySelector("h1");
 const h2 = document.querySelector("h2");
@@ -8,4 +10,11 @@ const h3 = document.querySelector("h3");
 const p = document.querySelector("p");
 const div = document.querySelector("div");
 // Recommended: Ask for the city name and then the rest of the code
+
+const targetCityName = prompt ("Vilken stad?");
+
+for (let city of cities) {
+    citiesDiv.innerHTML+= `<p class="cityBox">${city.name}</>`;
+}
+
 
