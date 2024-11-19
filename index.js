56a92aee1698b17213bb5c27e053e877e0e7058e
Moakarlssonns <mo-karlsson@hotmail.com>
2024-11-19 14:01:40 +0100

    Added if and else statements in the for of loop
    This causes the <h2> to change to the cities and countries that exist in the array. If you write a country in the prompt that doesn't exist in the array, the following message in <h2> will appear: "(city) (country) finns inte i databasen".

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 
 // Recommended: constants with references to existing HTML-elements
+const changeTabName = document.querySelector("title");
 const citiesDiv = document.getElementById("cities");
 const main = document.querySelector("main");
 const h1 = document.querySelector("h1");
@@ -12,9 +13,21 @@ const div = document.querySelector("div");
 // Recommended: Ask for the city name and then the rest of the code
 
 const targetCityName = prompt ("Vilken stad?");
-
 for (let city of cities) {
     citiesDiv.innerHTML+= `<p class="cityBox">${city.name}</>`;
+    if (targetCityName == city.name) {
+        h2.textContent = city.name + " " + "(" + city.country + ")";
+    } else {
+        h2.textContent = targetCityName + "finns inte i databasen";
+    }  
 }
+ 
+
+
+
+
+
+
+
 
 
