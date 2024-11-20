8984cb70f335957f13d5bcb61c408d78f44ba4c8
Moakarlssonns <mo-karlsson@hotmail.com>
2024-11-20 12:56:46 +0100

    Made first version of the table

--- index.js
+++ index.js
@@ -9,30 +9,56 @@ function cityContainer (targetCityName) {
 }
 
 // Recommended: constants with references to existing HTML-elements
-const changeTabName = document.querySelector("title");
 const citiesDiv = document.getElementById("cities");
 const main = document.querySelector("main");
 const h1 = document.querySelector("h1");
-const h2 = document.querySelector("h2");
 const h3 = document.querySelector("h3");
 const p = document.querySelector("p");
 const div = document.querySelector("div");
+const tablediv = document.getElementById("table");
 // Recommended: Ask for the city name and then the rest of the code
 
 const targetCityName = prompt ("Vilken stad?");
+const matchingCity = cityContainer(targetCityName);
+
 
 if (cityContainer(targetCityName) == null) {
-    h2.textContent = targetCityName + " finns inte i databasen! ";
+    const h2 = document.querySelector("h2");
+    h2.textContent = `${targetCityName} finns inte i databasen!`
+    document.title = `${targetCityName} - Stad finns inte i databasen!`; 
 } else {
-    h2.textContent = targetCityName + " (" + cityContainer(targetCityName).country + ")";
+    h2.textContent = `${matchingCity.name} ${cityContainer(targetCityName).country}`;
+    document.title = `${matchingCity.name} - ${matchingCity.country}`; 
 }
 for (let city of cities) {
-    citiesDiv.innerHTML+= `<p class="cityBox">${city.name}</p>`;
-    
+    citiesDiv.innerHTML += `<p class="cityBox">${city.name}</p>`; 
+}
+
+tablediv.innerHTML = `<p class = "cell"></p>`;
+
+for (let city of cities){
+    tablediv.innerHTML += `<p class ="cell head_row">${city.id}</p>`;
 }
 
 
- 
+for (let city of cities) {
+    tablediv.innerHTML += `<p class= "cell head_column">${city.id}-${city.name}</p>`;
+    tablediv.innerHTML += `<p class ="cell"></p>`;
+    for (let keys of distances) {
+        const pCell = document.createElement("p");
+        if (keys.city1 == city.id || keys.city2 == city.id) {
+            tablediv.appendChild(pCell);
+            pCell.classList.add("cell");
+            pCell.textContent = keys.distance / 10 ;
+        }
+       
+    }
+  
+}
+
+
+
+
 
 
 
